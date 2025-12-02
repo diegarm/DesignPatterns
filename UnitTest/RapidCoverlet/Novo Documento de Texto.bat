af9e698ca70165df93d93069bd247b8a3492b86b
Diego Armando <diego.arruda-bento@vinci-energies.net>
2025-12-02 10:23:47 +0000

    Update

--- UnitTest/RapidCoverlet/Novo Documento de Texto.bat
+++
@@ -1,2 +0,0 @@
-C:\Users\darmando\Documents\Pessoal\DesignPatterns\UnitTest\RapidCoverlet\ReportGenerator\ReportGenerator.exe "-reports:coverage.opencover.xml" "-targetdir:coveragereport" -reporttypes:Html
-pause
